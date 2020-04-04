819de4f5c8c605619ddf26c5f32d4a7478e7e293
Alexandr Shevchenko <ls.shov@gmail.com>
2020-04-04 21:22:42 +0300

    Updated, added selenium, fixed cron and installing deps

--- docker_env/python/install_deps.sh
+++ docker_env/python/install_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Deps installer";
+
 if [ ! -d /app/docker_env/docker_lock ]; then
  mkdir -p /app/docker_env/docker_lock
  echo "Install deps service: /app/dev/docker_lock notfound, installing..."
@@ -9,5 +11,7 @@ if [ ! -d /app/docker_env/docker_lock ]; then
   && source dev/bin/activate \
   && pip3 install -r requirements.txt \
   && pip3 install --upgrade git+https://github.com/m-wrzr/populartimes \
-  && mkdir -p /app/docker_env/docker_deps_installed
+  && mkdir -p /app/docker_env/docker_deps_installed;
+else
+  echo "Found /app/dev/docker_lock, no need to install deps, terminating";
 fi;
