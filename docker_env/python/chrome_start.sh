5ec23d6aecdf1c6f7ebe2518f62f3633af5f83f4
shukal94 <sergey.shukalovich@gmail.com>
2020-04-17 17:09:27 +0300

    path

--- docker_env/python/chrome_start.sh
+++ docker_env/python/chrome_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start selenium";
-
+export PATH="$PATH:/opt/google/chrome"
 cd /app \
  && java -jar  -Dselenium.LOGGER=selenium.log  -Dwebdriver.chrome.driver=/app/standalone/chromedriver /app/standalone/selenium-server-standalone-3.141.59.jar;
 
