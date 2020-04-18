5b880fff6f34e504ee6cad9cb753103e5e9b0877
Alexandr Shevchenko <ls.shov@gmail.com>
2020-04-18 14:16:37 +0300

    Extended script, clean up crontab

--- scripts/start_parser.sh
+++ scripts/start_parser.sh
@@ -3,5 +3,9 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd "${SCRIPT_PATH}/.."
-source ./dev/bin/activate
-python -m api.main && echo "Parsing done well"
+if [ -d /app/docker_env/docker_deps_installed ]; then
+  source ./dev/bin/activate
+  python -m api.main && echo "Parsing done well"
+else
+  echo "Dependencies are not installed yet!"
+fi;
