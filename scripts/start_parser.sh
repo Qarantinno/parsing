ec685dfcb19039cd2c2e95ac31e31e6433479ac7
Alexandr Shevchenko <ls.shov@gmail.com>
2020-04-19 00:46:57 +0300

    Updated star_parser script, source environment for stupid cron

--- scripts/start_parser.sh
+++ scripts/start_parser.sh
@@ -2,6 +2,16 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+#Dot env
+if [[ ! -f "${SCRIPT_PATH}/../docker_env/.env" ]]; then
+    echo ".env file not found! Process was stopped";
+    exit 1
+else
+  set -a
+  . "${SCRIPT_PATH}/../docker_env/.env"
+  set +a
+fi;
+
 cd "${SCRIPT_PATH}/.."
 if [ -d /app/docker_env/docker_deps_installed ]; then
   source ./dev/bin/activate
